f654ca868bc9460d2bcdbea295b238f82178e776
Gabriel Cardona <cgcardona@gmail.com>
2019-12-06 15:50:15 -0800

    Syntax fix.

--- src/index.js
+++ src/index.js
@@ -1,65 +1,63 @@
 require("source-map-support/register");
 
-const Command = require('./lib/command')
-const TaskError = require('./lib/errors/taskerror')
-const TruffleError = require('@truffle/error')
-const version = require('./lib/version')
-const OS = require('os')
-const downloader = require('./downloader')
+const Command = require("./lib/command");
+const TaskError = require("./lib/errors/taskerror");
+const TruffleError = require("@truffle/error");
+const version = require("./lib/version");
+const OS = require("os");
+const downloader = require("./downloader");
 
-const command = new Command(require('./lib/commands'))
+const command = new Command(require("./lib/commands"));
 
 const options = {
   logger: console
-}
+};
 
-const commands = process.argv.slice(2)
-
-if (commands[0] === '--download-compiler' && commands[1]) {
-
-  downloader(commands[1])
+const commands = process.argv.slice(2);
 
 if (commands[0] === "--download-compiler" && commands[1]) {
   downloader(commands[1]);
 } else {
-  let command = new Command(require("./lib/commands"));
-
-  command.run(process.argv.slice(2), options, function (err) {
+  command.run(process.argv.slice(2), options, function(err) {
     if (err) {
       if (err instanceof TaskError) {
         command.args
-          .usage('Earthcli v' + (version.bundle || version.core) + ' - a development framework for earthweb'
-            + OS.EOL + OS.EOL
-            + 'Usage: earthcli <command> [options]')
-          .epilog('See more at https://www.earth.engineering')
-          .showHelp()
+          .usage(
+            "Earthcli v" +
+              (version.bundle || version.core) +
+              " - a development framework for earthweb" +
+              OS.EOL +
+              OS.EOL +
+              "Usage: earthcli <command> [options]"
+          )
+          .epilog("See more at https://www.earth.engineering")
+          .showHelp();
       } else {
         if (err instanceof TruffleError) {
-          console.error(err.message)
-        } else if (typeof err === 'number') {
+          console.error(err.message);
+        } else if (typeof err === "number") {
           // If a number is returned, exit with that number.
           // eslint-disable-next-line no-process-exit
-          process.exit(err)
+          process.exit(err);
         } else {
           // Bubble up all other unexpected errors.
-          console.error(err.stack || err.toString())
+          console.error(err.stack || err.toString());
         }
       }
       // eslint-disable-next-line no-process-exit
-      process.exit(1)
+      process.exit(1);
     }
 
     // Don't exit if no error; if something is keeping the process open,
-    // like `earthcli console`, then let it.
+    // like `truffle console`, then let it.
 
     // Clear any polling or open sockets - `provider-engine` in HDWallet
     // and `web3 1.0 confirmations` both leave interval timers etc wide open.
-    const handles = process._getActiveHandles()
+    const handles = process._getActiveHandles();
     handles.forEach(handle => {
-      if (typeof handle.close === 'function') {
-        handle.close()
+      if (typeof handle.close === "function") {
+        handle.close();
       }
-    })
-  })
-
+    });
+  });
 }
